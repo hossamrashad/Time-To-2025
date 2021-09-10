64408cbc4d65726744879274eac28d0a85899439
hossamrashad <bibo2010508@yahoo.com>
2021-09-10 04:04:40 +0200

    daysEl

--- js/script.js
+++ js/script.js
@@ -6,7 +6,6 @@ const secondsEl = document.getElementById("seconds");
 const newYears = "1 jan 2025";
 
 function countdown() {
-
     const newYearsDate = new Date(newYears);
 
     const currentDate = new Date();
@@ -21,13 +20,45 @@ function countdown() {
     hoursEl.innerHTML = formatTime(hours);
     minutesEl.innerHTML = formatTime(minutes);
     secondsEl.innerHTML = formatTime(seconds);
+    if (seconds < 50) {
+        secondsEl.style.color = "cadetblue";
+        minutesEl.style.color = "cadetblue";
+        hoursEl.style.color = "cadetblue";
+        daysEl.style.color = "cadetblue";
 
+    }
+    if (seconds < 40) {
+        secondsEl.style.color = "#3498da";
+        minutesEl.style.color = "#3498da";
+        hoursEl.style.color = "#3498da";
+        daysEl.style.color = "#3498da";
+
+    }
+    if (seconds < 30) {
+        secondsEl.style.color = "#f44336";
+        minutesEl.style.color = "#f44336";
+        hoursEl.style.color = "#f44336";
+        daysEl.style.color = "#f44336";
+
+    }
+    if (seconds < 20) {
+        secondsEl.style.color = "#4caf50";
+        minutesEl.style.color = "#4caf50";
+        hoursEl.style.color = "#4caf50";
+        daysEl.style.color = "#4caf50";
+
+    }
+    if (seconds < 10) {
+        secondsEl.style.color = "#ffeb3b";
+        minutesEl.style.color = "#ffeb3b";
+        hoursEl.style.color = "#ffeb3b";
+        daysEl.style.color = "#ffeb3b";
+
+    }
 }
 
 function formatTime(time) {
-
     return time < 10 ? `0${time}` : time;
-
 }
 
 countdown();
